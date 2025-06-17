d21402aa1456fe65bbe7bc4cdd72f4c3c53db24d
Roberto Leogrande <boris47@hotmail.it>
2025-06-17 09:34:45 +0200

    Clear folder

--- src/main/java/com/example/jsonmodifier/ParentClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.jsonmodifier;
-
-public abstract class ParentClass
-{
-	public void Method()
-	{
-		System.out.println("ParentClass");
-	}
-}
--- src/main/java/com/example/userservice/Util/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.userservice.Util;
-
-public class StringUtils
-{
-    public static boolean isNullOrEmpty(String input)
-    {
-        return input == null || input.isEmpty();
-    }
-}
